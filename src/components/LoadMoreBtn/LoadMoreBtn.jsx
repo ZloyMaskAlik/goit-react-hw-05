a14d32d7d5036e90ad18f8f2a3790d6f2a920e4d
zloymaskalik <usvplus@gmail.com>
2025-04-02 17:14:20 +0300

    react-hw-05

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++
@@ -1,9 +0,0 @@
-import css from './LoadMoreBtn.module.css';
-
-export default function LoadMoreBtn ({ onLoadMore }){
-  return (
-    <button className={css.button} type="button" onClick={onLoadMore}>
-      Load more
-    </button>
-  );
-};
